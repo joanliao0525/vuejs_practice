08563d2dc1365820906900713f319ab5739139bb
joanliao0525 <comeon19940525@hotmail.com>
2019-10-31 16:24:41 +0800

    vuejs v-model use

---
+++ 6/js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        // gender: []
+        // article: 'Laboris magna exercitation velit deserunt nostrud.Lorem non laboris sint ullamco. Proident aliqua nostrud reprehenderit ullamco et. Culpa amet velit laborum adipisicing. Laboris veniam duis consectetur ex proident eiusmod occaecat nulla. Irure ut ullamco proident mollit et nisi id esse ea. Dolor qui velit est tempor anim magna sit dolore voluptate quis exercitation laboris.'
+        where: null,
+        dest: [],
+    },
+});
